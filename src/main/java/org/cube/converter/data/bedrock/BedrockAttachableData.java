09b54638f578bfea7e91cc6f5b3b43721f9152c0
boybook <fangyizhou1997@qq.com>
2026-02-26 11:11:06 +0800

    Add attachable and entity data parsing improvements

--- src/main/java/org/cube/converter/data/bedrock/BedrockAttachableData.java
+++ src/main/java/org/cube/converter/data/bedrock/BedrockAttachableData.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 public class BedrockAttachableData extends BedrockEntityData {
-    public BedrockAttachableData(String identifier, Scripts scripts, List<RenderController> controllers, Map<String, String> materials, Map<String, String> animations, Map<String, String> textures, Map<String, String> geometries) {
-        super(identifier, scripts, controllers, materials, animations, textures, geometries);
+    public BedrockAttachableData(String identifier, Scripts scripts, List<RenderController> controllers, Map<String, String> materials, Map<String, String> animations, Map<String, String> textures, Map<String, String> geometries, Map<String, String> particleEffects) {
+        super(identifier, scripts, controllers, materials, animations, textures, geometries, particleEffects);
     }
 }
